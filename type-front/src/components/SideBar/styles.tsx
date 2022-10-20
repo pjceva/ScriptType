30e95e2f72dba3e52ca416c660eccc84f7743979
pjceva <pjceva@gmail.com>
2022-10-20 11:37:02 -0300

    Alteracoes esteticas sidebar

--- type-front/src/components/SideBar/styles.tsx
+++ type-front/src/components/SideBar/styles.tsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Container = styled.section`
     height: 100vh;
     width: 15%;
-    background: rgba(217, 217, 217, 0.1);
+    /* background: rgba(217, 217, 217, 0.1); */
+    background: rgba(41, 228, 183, 0.05);
     position: absolute;
     display: flex;
     flex-direction: column;
@@ -25,7 +26,8 @@ export const Body = styled.section`
 `
 
 export const Bar = styled.section`
-    background: rgba(41, 228, 183, 0.15);
+    /* background: rgba(41, 228, 183, 0.15); */
+    background: rgba(41, 228, 183, 0.35);
     padding: 15px 5px;
     font-size: 1.4rem;
 
@@ -34,11 +36,12 @@ export const Bar = styled.section`
 export const SubSection = styled.section`
     width: 80%;
     margin: 10px auto;
-    border-bottom: 1px solid white;
+    border-bottom: 2px solid white;
     font-size: 1.4rem;
     padding: 3px 10px;
     display: flex;
     justify-content: space-between;
+    color: white;
     span{
         font-size: 1rem;
     }
